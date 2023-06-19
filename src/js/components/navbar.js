d4bef2b3dc5d32c9bb490b6334934a1540fa5ae4
Nour El Din Shady <rhasoldy@gmail.com>
2023-06-20 01:22:44 +0300

    Added new about section, sand separators and working mobile nav

--- src/js/components/navbar.js
+++ src/js/components/navbar.js
@@ -1,6 +1,13 @@
-const nav = document.querySelector(".navbar-list");
-const expandBtn = document.querySelector("#expand-btn");
+const nav = document.querySelector("nav");
+const navbarList = document.querySelector(".navbar-list");
+const expandBtn = document.querySelector("#btn-expand");
 
-// expandBtn.addEventListener("click", () => {
-//   nav.classList.toggle("navbar-close-menu");
-// });
+expandBtn.addEventListener("click", () => {
+  nav.classList.toggle("burger-menu");
+  if (nav.classList.contains("burger-menu")) {
+    nav.style.mixBlendMode = "initial";
+  } else {
+    nav.style.mixBlendMode = "difference";
+  }
+  navbarList.classList.toggle("burger-list");
+});
