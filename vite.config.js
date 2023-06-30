f6558cd299f4d852772e59b8328f47ac84f87f0e
Nour El Din Shady <rhasoldy@gmail.com>
2023-06-30 23:39:52 +0300

    Fixed multipages in building

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   root: "./src",
   build: {
     target: "esnext",
     outDir: "../dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        pages: resolve(__dirname, "src/pages/booking.html")
+      }
+    }
   },
   publicDir: "../public",
 });
