61a01749b71a3231c1150ecb53278ed26dad7b06
Nour El Din Shady <rhasoldy@gmail.com>
2023-07-11 05:51:27 +0300

    Fixed html

--- src/js/components/createImageCards.js
+++ src/js/components/createImageCards.js
@@ -1,11 +1,9 @@
 export const createImageCard = function (imageUrl) {
-  const imageCard = `
-	<img
+  const imageCard = `<img
   class="image-card shadow"
   src="${imageUrl}"
   alt="Picture of the Kaaba"
-  />
-	`;
+  />`;
 
   const carousel = document.querySelector("#gallery-carousel");
   carousel?.insertAdjacentHTML("beforeend", imageCard);
